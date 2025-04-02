2623f8415e5c5af3e4f790864c49bcde346e29f6
oden-35 <dashengdachu@gmail.com>
2025-04-02 14:47:42 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -1,18 +1,27 @@
 <?php
+function sort_2way(array &$nums, bool $order): void {
+    if ($order) {
+        // 昇順ソート
+        sort($nums);
+    } else {
+        // 降順ソート
+        rsort($nums);
+    }
+}
+
+// テスト
 $nums = [15, 4, 18, 23, 10];
 
-// 昇順ソート
-sort($nums);
-echo "昇順にソートします\n";
+echo "昇順にソートします:\n";
+sort_2way($nums, true); // 昇順
 foreach ($nums as $num) {
     echo $num . "\n";
 }
 
 echo "\n"; // 改行
 
-// 降順ソート
-rsort($nums);
-echo "降順にソートします\n";
+echo "降順にソートします:\n";
+sort_2way($nums, false); // 降順
 foreach ($nums as $num) {
     echo $num . "\n";
 }
